51c5ab013e1e45eaac89c4052abcdb64fd3f28ce
NicolaGugole <nicola.gugole@gmail.com>
2020-12-02 16:07:20 +0100

    added monkas dataset and primordial script

---
+++ scripts/monk1_model.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Neuron
+class Neuron:
+    def _init_(self):
+        pass
+
+# Layer
+class Layer:
+    def _init_(self):
+        pass
+
+# Activation Function
+class Act_Func:
+    def _init_(self):
+        pass
+
+
+# ----------------------------------------- MAIN ----------------------------------------- #
+
+
+print("One day I will be a very smart Artificial Intelligence!")
