d8df272dd50c4f96517921a1c76130b6abe39257
NicolaGugole <nicola.gugole@gmail.com>
2020-12-17 15:50:22 +0100

    model, STONKS

---
+++ scripts/utils/model.py
@@ -0,0 +1,11 @@
+class Model:
+
+    def __init__(self, layers):
+        """
+        Contructor of Model class
+
+        Parameters:
+        layers(Layer): list of layers which compose the model
+        
+        """
+        self.layers = layers
