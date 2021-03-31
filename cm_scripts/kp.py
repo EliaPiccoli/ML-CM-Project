dbc7442527b56099cce70dea4748d5c43e506d8e
EliaPiccoli <eliakoby@live.it>
2021-03-31 15:39:28 +0200

    median of medians

--- cm_scripts/kp.py
+++ cm_scripts/kp.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math
-import copy
 
 def generate_all_mu(betas, box):
     M = [] # 0 - mu_u | 1 - mu_l
@@ -12,11 +11,11 @@ def median_of_medians(elems):
     sublists = [elems[j:j+5] for j in range(0, len(elems), 5)]     
     medians = []     
     for sublist in sublists:         
-        medians.append(sorted(sublist)[len(sublist)/2])     
-        if len(medians) <= 5:         
-            return sorted(medians)[len(medians)/2]     
-        else:         
-            return median_of_medians(medians) 
+        medians.append(sorted(sublist)[len(sublist)//2])     
+    if len(medians) <= 5:         
+        return sorted(medians)[len(medians)//2]     
+    else:         
+        return median_of_medians(medians) 
 
 def generate_betas(mu, betas, box, M):
     new_betas = []
@@ -37,12 +36,7 @@ def lin_interp(mu_L, mu_U, betas, box, M):
 def adjust_M(M, mu, mode):
     # mode 0 | retain only all mu_l and mu_u bigger than mu (sum is too high, we need to lower it)
     # mode 1 | retain only all mu_l and mu_u lower than mu (sum is too low, we need to higher it)
-    if mode == 0:
-        return M[M > mu]
-    else:
-        return M[M < mu]
-
-
+    return M[M > mu] if mode == 0 else M[M < mu]
 
 def solveKP(box, linear_constraint, betas):
     # box - range constraining the betas (-box box) = (-C C)
@@ -51,19 +45,16 @@ def solveKP(box, linear_constraint, betas):
     original_M = generate_all_mu(betas, box) # structured list - each element is a pair (0 for mu_u and 1 for mu_l)
     mu_L, mu_U = math.inf, -math.inf
     M = np.ravel(original_M) # for keeping all mu_i_l and mu_i_u protected from the loop variations
-    print(f"INITIAL BETAS: {betas}\n INITIAL mu_L: {mu_L}\nINITIAL mu_U: {mu_U}")
+    print(f"\nINITIAL BETAS: {betas}\n INITIAL mu_L: {mu_L}\nINITIAL mu_U: {mu_U}")
     while M.size != 0:
-        print(f"M: {M}")
-        #mu = median_of_medians(M) # need a fix..
-        sorted_M = sorted(M)
-        mu = sorted(M)[M.size//2]
-        print(f"MEDIAN OF {sorted_M} IS {mu}")
+        mu = median_of_medians(M)
+        print(f"\nMEDIAN OF {M} IS {mu}")
         temp_betas = generate_betas(mu, betas, box, original_M)
         betas_sum = np.sum(temp_betas)
-        print(f"NEW BETAS: {temp_betas}")
-        print(f"SUM OF BETAS: {betas_sum}")
+        print(f"\nNEW BETAS: {temp_betas}")
+        print(f"\nSUM OF BETAS: {betas_sum}")
         if betas_sum == linear_constraint:
-            print("LUCKY ESCAPE!")
+            print("\nLUCKY ESCAPE!")
             return temp_betas
         elif betas_sum > linear_constraint:
             mu_L = mu
@@ -71,19 +62,16 @@ def solveKP(box, linear_constraint, betas):
         else:
             mu_U = mu
             M = adjust_M(M, mu, 1)
-        print(f"END OF ITER mu_L {mu_L} AND mu_U {mu_U}")
+        print(f"\nEND OF ITER mu_L {mu_L} AND mu_U {mu_U}")
     # if we arrive here then solution stands in linear interpolation
-    print("SOLUTION FOUND BY LINEAR INTERPOLATION")
+    print("\nSOLUTION FOUND BY LINEAR INTERPOLATION")
     mu = lin_interp(mu_L, mu_U, betas, box, original_M)
     return generate_betas(mu, betas, box, original_M)
-
-
-
 
 if __name__ == "__main__":
     C = 3
     box = C
     linear_constraint = 0
-    betas = np.random.uniform(-1,1,1000)
+    betas = np.random.uniform(-1,1,1530)
     betas = solveKP(box, linear_constraint, betas)
     print(f"FINAL BETAS: {betas}\nAND FINAL SUM OF BETAS: {np.sum(betas)}")
